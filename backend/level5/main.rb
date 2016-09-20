df91d1eea234478ad86b202e0f2021248053b08a
Vincent Molinié <molinie.vincent@gmail.com>
2016-09-21 00:18:23 +0200

    Refactoring code of level5

--- backend/level5/main.rb
+++ backend/level5/main.rb
@@ -10,8 +10,7 @@ data['cars'].each do |car|
 	pricesPerCar[car['id']] = {pricePerDay: car['price_per_day'], pricePerKm: car['price_per_km']}
 end
 
-output = {rentals: []}
-data['rentals'].each do |rental|
+def calculateRentalPrices(rental, pricesPerCar)
 	distancePrice = pricesPerCar[rental['car_id']][:pricePerKm] * rental['distance']
 	rentalDuration = Date.parse(rental['end_date']) - Date.parse(rental['start_date'])
 	rentalDuration = rentalDuration.ceil + 1
@@ -35,17 +34,29 @@ data['rentals'].each do |rental|
 	drivyFee = commission - insuranceFee - assistanceFee
 	deductibleReduction = rental['deductible_reduction'] ? 400 * rentalDuration : 0
 
-	rentalPayments = {id: rental['id'], actions: []}
+	return {id: rental['id'], price: price,
+		options: {deductible_reduction: deductibleReduction },
+	 	commission: {insurance_fee: insuranceFee, assistance_fee: assistanceFee, drivy_fee: drivyFee}}
+end
 
-	rentalPayments[:actions] << {who: 'driver', type: 'debit', amount: price + deductibleReduction}
-	rentalPayments[:actions] << {who: 'owner', type: 'credit', amount: price - commission}
-	rentalPayments[:actions] << {who: 'insurance', type: 'credit', amount: insuranceFee}
-	rentalPayments[:actions] << {who: 'assistance', type: 'credit', amount: assistanceFee}
-	rentalPayments[:actions] << {who: 'drivy', type: 'credit', amount: drivyFee + deductibleReduction}
+def calculateActionsForRental(rentalPrices)
+	rentalActions = {id: rentalPrices[:id], actions: []}
 
+	rentalActions[:actions] << {who: 'driver', type: 'debit', amount: rentalPrices[:price] + rentalPrices[:options][:deductible_reduction]}
+	rentalActions[:actions] << {who: 'owner', type: 'credit', amount: (rentalPrices[:price] * 0.7).ceil}
+	rentalActions[:actions] << {who: 'insurance', type: 'credit', amount: rentalPrices[:commission][:insurance_fee]}
+	rentalActions[:actions] << {who: 'assistance', type: 'credit', amount: rentalPrices[:commission][:assistance_fee]}
+	rentalActions[:actions] << {who: 'drivy', type: 'credit', amount: rentalPrices[:commission][:drivy_fee] + rentalPrices[:options][:deductible_reduction]}
 
+	return rentalActions
+end
 
-	output[:rentals] << rentalPayments
+output = {rentals: []}
+data['rentals'].each do |rental|
+	rentalPrices = calculateRentalPrices(rental, pricesPerCar)
+	rentalActions = calculateActionsForRental(rentalPrices)
+
+	output[:rentals] << rentalActions
 end
 
 jsonOutput = JSON.pretty_generate(output)
