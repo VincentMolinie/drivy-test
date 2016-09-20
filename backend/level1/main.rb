2a4ba6db0fbce43caabd6dea6d44ad9a7fe4b029
Vincent Molinié <molinie.vincent@gmail.com>
2016-09-20 21:16:00 +0200

    Level 1 done

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,23 @@
 require "json"
+require "date"
 
 # your code
+dataFile = File.read('data.json')
+data = JSON.parse(dataFile)
+
+pricesPerCar = {}
+data['cars'].each do |car|
+	pricesPerCar[car['id']] = {pricePerDay: car['price_per_day'], pricePerKm: car['price_per_km']}
+end
+
+output = {rentals: []}
+data['rentals'].each do |rental|
+	distancePrice = pricesPerCar[rental['car_id']][:pricePerKm] * rental['distance']
+	rentalDuration = Date.parse(rental['end_date']) - Date.parse(rental['start_date'])
+	rentalDuration = rentalDuration.ceil + 1
+	dayPrice = pricesPerCar[rental['car_id']][:pricePerDay] * rentalDuration
+	output[:rentals] << {id: rental['id'], price: (dayPrice + distancePrice)}
+end
+
+jsonOutput = JSON.pretty_generate(output)
+File.write('output.json', jsonOutput)
