9ddf56d2e59ddce36094c047af8b427ae22dbe11
Vincent Molinié <molinie.vincent@gmail.com>
2016-09-22 16:31:22 +0200

    Level6 done with classes

--- backend/level6/main.rb
+++ backend/level6/main.rb
@@ -1,126 +1,174 @@
 require "json"
 require "date"
 
-# your code
+class Car
+	attr_accessor :id, :price_per_day, :price_per_km
+
+	def initialize(id, price_per_day, price_per_km)
+		@id = id
+		@price_per_day = price_per_day
+		@price_per_km = price_per_km
+	end
+end
+
+class Rental
+	attr_accessor :id, :car, :start_date, :end_date, :distance, :deductible_reduction_active
+	attr_reader :price, :assistance_fee, :insurance_fee, :drivy_fee, :deductible_reduction
+	attr_reader :driver_payment, :owner_payment, :insurance_payment, :assistance_payment, :drivy_payment
+
+	def initialize(id, car, start_date, end_date, distance, deductible_reduction_active = false)
+		@id = id
+		@car = car
+		@start_date = start_date
+		@end_date = end_date
+		@distance = distance
+		@deductible_reduction_active = deductible_reduction_active
+	end
+
+	def calculatePrices
+		distance_price = @car.price_per_km * @distance
+		rental_duration = Date.parse(@end_date) - Date.parse(@start_date)
+		rental_duration = rental_duration.ceil + 1
+
+		if rental_duration > 10
+			day_price = ((rental_duration - 10) * 0.5 + 6 * 0.7 + 3 * 0.9 + 1) * @car.price_per_day
+		elsif rental_duration > 4
+			day_price = ((rental_duration - 4) * 0.3 + 3 * 0.9 + 1) * @car.price_per_day
+		elsif rental_duration > 1
+			day_price = ((rental_duration - 1) * 0.9 + 1) * @car.price_per_day
+		else
+			day_price = @car.price_per_day
+		end
+				
+		day_price = day_price.ceil
+		@price = day_price + distance_price
+		commission = (price * 0.3).ceil
+		
+		@insurance_fee = (commission * 0.5).ceil
+		@assistance_fee = rental_duration * 100
+		@drivy_fee = commission - insurance_fee - assistance_fee
+		@deductible_reduction = @deductible_reduction_active ? 400 * rental_duration : 0
+	end
+
+	def calculateActions
+		if !@price
+			calculatePrices
+		end
+
+		@driver_payment = @price + @deductible_reduction
+		@owner_payment = (@price * 0.7).ceil
+		@insurance_payment = @insurance_fee
+		@assistance_payment = @assistance_fee
+		@drivy_payment = @drivy_fee + @deductible_reduction
+	end
+
+	def outputPrices
+		if !@price
+			calculatePrices
+		end
+
+		output = {id: @id, price: @price,
+		 	options: {deductible_reduction: @deductible_reduction},
+			commission: {insurance_fee: @insurance_fee, assistance_fee: @assistance_fee, drivy_fee: @drivy_fee}}
+		
+		return output
+	end
+
+	def outputActions
+		if !@driver_payment
+			calculateActions
+		end
+
+		output = {id: @id, actions: []}
+		output[:actions] << {who: 'driver', type: @driver_payment < 0 ? 'credit' : 'debit', amount: @driver_payment.abs}
+		output[:actions] << {who: 'owner', type: @owner_payment < 0 ? 'debit' : 'credit', amount: @owner_payment.abs}
+		output[:actions] << {who: 'insurance', type: @insurance_payment < 0 ? 'debit' : 'credit', amount: @insurance_payment.abs}
+		output[:actions] << {who: 'assistance', type: @assistance_payment < 0 ? 'debit' : 'credit', amount: @assistance_payment.abs}
+		output[:actions] << {who: 'drivy', type: @drivy_payment < 0 ? 'debit' : 'credit', amount: @drivy_payment.abs}
+	
+		return output
+	end
+end
+
+class RentalModification < Rental
+	attr_accessor :rental
+
+	def initialize(id, rental, start_date = nil, end_date = nil, distance = nil)
+		@id = id
+		@rental = rental
+
+		@start_date = start_date ? start_date : @rental.start_date
+		@end_date = end_date ? end_date : @rental.end_date
+		@distance = distance ? distance : @rental.distance
+		@car = @rental.car
+		@deductible_reduction_active = @rental.deductible_reduction_active
+	end
+
+	def calculatePrices
+		if !@rental.price
+			@rental.calculatePrices
+		end
+
+		method(:calculatePrices).super_method.call
+		@price -= @rental.price
+		@assistance_fee -= @rental.assistance_fee
+		@insurance_fee -= @rental.insurance_fee
+		@drivy_fee -=@rental.drivy_fee
+		@deductible_reduction -= @rental.deductible_reduction
+	end
+
+	def calculateActions
+		if !@price
+			calculatePrices
+		end
+
+		method(:calculateActions).super_method.call
+	end
+
+	def outputPrices
+		output = method(:outputPrices).super_method.call
+		output['rental_id'] = @rental.id
+		return output
+	end
+
+	def outputActions
+		if !@driver_payment
+			calculateActions
+		end
+
+		output = method(:outputActions).super_method.call
+		output['rental_id'] = @rental.id
+		return output
+	end
+end
+
 dataFile = File.read('data.json')
 data = JSON.parse(dataFile)
 
-pricesPerCar = {}
+#Import the data
+carsHash = {}
 data['cars'].each do |car|
-	pricesPerCar[car['id']] = {pricePerDay: car['price_per_day'], pricePerKm: car['price_per_km']}
+	carsHash[car['id']] = Car.new(car['id'], car['price_per_day'], car['price_per_km'])
 end
 
-def calculateRentalPrices(rental, pricesPerCar)
-	distancePrice = pricesPerCar[rental['car_id']][:pricePerKm] * rental['distance']
-	rentalDuration = Date.parse(rental['end_date']) - Date.parse(rental['start_date'])
-	rentalDuration = rentalDuration.ceil + 1
-
-	pricePerDay = pricesPerCar[rental['car_id']][:pricePerDay]
-	if rentalDuration > 10
-		dayPrice = ((rentalDuration - 10) * 0.5 + 6 * 0.7 + 3 * 0.9 + 1) * pricePerDay
-	elsif rentalDuration > 4
-		dayPrice = ((rentalDuration - 4) * 0.3 + 3 * 0.9 + 1) * pricePerDay
-	elsif rentalDuration > 1
-		dayPrice = ((rentalDuration - 1) * 0.9 + 1) * pricePerDay
-	else
-		dayPrice = pricePerDay
-	end
-			
-	dayPrice = dayPrice.ceil
-	price = dayPrice + distancePrice
-	commission = (price * 0.3).ceil
-	insuranceFee = (commission * 0.5).ceil
-	assistanceFee = rentalDuration * 100
-	drivyFee = commission - insuranceFee - assistanceFee
-	deductibleReduction = rental['deductible_reduction'] ? 400 * rentalDuration : 0
-
-	return {id: rental['id'], price: price,
-		options: {deductible_reduction: deductibleReduction },
-	 	commission: {insurance_fee: insuranceFee, assistance_fee: assistanceFee, drivy_fee: drivyFee}}
+rentalsHash = {}
+data['rentals'].each do |rental|
+	car = carsHash[rental['car_id']]
+	rentalsHash[rental['id']] = Rental.new(rental['id'], car, rental['start_date'], rental['end_date'], rental['distance'], rental['deductible_reduction'])
 end
 
-def calculateActionsForRental(rentalPrices)
-	rentalActions = {id: rentalPrices[:id], actions: []}
-
-	driverPayment = rentalPrices[:price] + rentalPrices[:options][:deductible_reduction]
-	rentalActions[:actions] << {who: 'driver', type: driverPayment < 0 ? 'credit' : 'debit', amount: driverPayment.abs}
-	ownerPayment = (rentalPrices[:price] * 0.7).ceil
-	rentalActions[:actions] << {who: 'owner', type: ownerPayment < 0 ? 'debit' : 'credit', amount: ownerPayment.abs}
-	insurancePayment = rentalPrices[:commission][:insurance_fee]
-	rentalActions[:actions] << {who: 'insurance', type: insurancePayment < 0 ? 'debit' : 'credit', amount: insurancePayment.abs}
-	assistancePayment = rentalPrices[:commission][:assistance_fee]
-	rentalActions[:actions] << {who: 'assistance', type: assistancePayment < 0 ? 'debit' : 'credit', amount: assistancePayment.abs}
-	drivyPayment = rentalPrices[:commission][:drivy_fee] + rentalPrices[:options][:deductible_reduction]
-	rentalActions[:actions] << {who: 'drivy', type: drivyPayment < 0 ? 'debit' : 'credit', amount: drivyPayment.abs}
-
-	return rentalActions
+rentalsModificationHash = {}
+data['rental_modifications'].each do |rental_modification|
+	rental = rentalsHash[rental_modification['rental_id']]
+	rentalsModificationHash[rental_modification['id']] = RentalModification.new(rental_modification['id'], rental, rental_modification['start_date'],
+																			 rental_modification['end_date'], rental_modification['distance'])
 end
 
-def calculateRentalsPrices(rentals, pricesPerCar, isModification=false)
-	output = {rentals: []}
-	rentals.each do |rental|
-		rentalPrices = calculateRentalPrices(rental, pricesPerCar)
-		if isModification
-			rentalPrices[:rental_id] = rental['rental_id']
-		end
-		output[:rentals] << rentalPrices
-	end
-	return output
+
+output = {rental_modifications: []}
+rentalsModificationHash.each do |id, rental_modification|
+	output[:rental_modifications] << rental_modification.outputActions
 end
-
-def calculateRentalsActions(rentalsPrices, isModification=false)
-	output = isModification ? {rental_modifications: []} : {rentals: []}
-	rentalsPrices.each do |rentalPrices|
-		rentalActions = calculateActionsForRental(rentalPrices)
-		if isModification
-			rentalActions[:rental_id] = rentalPrices[:rental_id]
-			output[:rental_modifications] << rentalActions
-		else	
-			output[:rentals] << rentalActions
-		end
-	end
-	return output
-end
-
-def saveRentalsById(rentals)
-	rentalsById = {}
-	rentals.each do |rental|
-		rentalsById[rental['id']] = rental
-	end
-	return rentalsById
-end
-
-rentalsById = saveRentalsById(data['rentals'])
-initialRentalsPrices = calculateRentalsPrices(data['rentals'], pricesPerCar)
-initialRentals = calculateRentalsActions(initialRentalsPrices[:rentals])
-
-rentalWithModifications = []
-data['rental_modifications'].each do |rentalModification|
-	rental = rentalsById[rentalModification['rental_id']]
-	rentalWithModification = rental.clone
-
-	rentalModification.keys.each do |key|
-		rentalWithModification[key] = rentalModification[key]
-	end
-
-	rentalWithModifications << rentalWithModification
-end
-
-modificationRentalsPrices = calculateRentalsPrices(rentalWithModifications, pricesPerCar, true)
-
-#Calculate the difference
-modificationRentalsPrices[:rentals].each do |modificationRentalPrices|
-	rentalId = modificationRentalPrices[:rental_id]
-	initialRentalPrices = initialRentalsPrices[:rentals].select {|rental| rental[:id] == rentalId}.first
-	modificationRentalPrices[:price] -= initialRentalPrices[:price]
-	modificationRentalPrices[:options][:deductible_reduction] -= initialRentalPrices[:options][:deductible_reduction]
-	modificationRentalPrices[:commission][:assistance_fee] -= initialRentalPrices[:commission][:assistance_fee]
-	modificationRentalPrices[:commission][:insurance_fee] -= initialRentalPrices[:commission][:insurance_fee]
-	modificationRentalPrices[:commission][:drivy_fee] -= initialRentalPrices[:commission][:drivy_fee]
-end
-
-output = calculateRentalsActions(modificationRentalsPrices[:rentals], true)
-
 
 jsonOutput = JSON.pretty_generate(output)
 File.write('output.json', jsonOutput)
